28a0db0484cb3cabdc37c41a5283e6c6638368d8
roaked <ossunarola@gmail.com>
2023-11-18 17:37:11 +0100

    comment tensoes and solver

--- code/Solver.m
+++ code/Solver.m
@@ -1,38 +1,45 @@
 function [solucao_nodal] = Solver(K,F,cfronteira,n_nos)
-%funcao que devolve a solucao em cada nó
+% Function that returns the solution at each node
 
-nos_scf = n_nos-length(cfronteira);%nós sem condiçao de fronteira a 0
-Kred = zeros(nos_scf);%inicialização a zeros do Kred
-Fred = zeros(nos_scf,1);%inicialização a zeros do Fred
+nos_scf = n_nos - length(cfronteira); % Nodes without boundary conditions set to 0
+Kred = zeros(nos_scf); % Initializing the reduced stiffness matrix Kred to zeros
+Fred = zeros(nos_scf, 1); % Initializing the reduced force vector Fred to zeros
 
-vaux = [];                                           
-for p = 1:1:n_nos  %criação de um vetor que contém os índices dos nós em que não exixtem
-                    %condições de fronteira
-    o = find(cfronteira(:,1)==p,1);
+vaux = [];
+for p = 1:1:n_nos
+    % Creating a vector containing the indices of nodes without boundary conditions
+    o = find(cfronteira(:,1) == p, 1);
     if isempty(o)
         vaux = [vaux p];
     end
-end 
+end
 
-
-for i = 1:1:length(vaux) %percorre até ao nº de nós que não têm condição de fronteira
-    for p = 1:length(vaux) 
-        %neste ciclo é criada a matriz de rigidez Kred que corresponde à matriz global
-        %já sem as linhas e colunas onde as soluções nodais são nulas
-        Kred(i,p) = K(vaux(i),vaux(p)); 
-        
+for i = 1:1:length(vaux)
+    % Loop through nodes without boundary conditions
+    for p = 1:length(vaux)
+        % Within this loop, the reduced stiffness matrix Kred is created. This matrix corresponds to the global matrix 
+        % but excludes rows and columns where nodal solutions are zero
+        Kred(i,p) = K(vaux(i), vaux(p)); 
+        % Extracting elements from the global stiffness matrix to form the reduced stiffness matrix
     end
 end
-for z = 1:1:length(vaux) %neste ciclo é executado exatamente o mesmo processo mas                                          
-    Fred(z,1) = F(vaux(z),1);  %agora para o vetor de cargas global
-end                                            
-psis = (Kred^-1)*Fred;  %calcula da solução através da inversa da matriz global
-solucao_nodal = zeros(n_nos,1);
+
+
 for z = 1:1:length(vaux)
-    solucao_nodal(vaux(z)) = psis(z); %criação e apresentação do vetor com a solução total 
-                                      %(incluindo os nós em que esta é nula)
+    % Loop through nodes without boundary conditions
+    Fred(z, 1) = F(vaux(z), 1);
+    % Building the reduced force vector Fred by selecting values corresponding to nodes without boundary conditions
 end
 
+psis = (Kred^-1) * Fred;
+% Solving for the solution through matrix inversion: using the inverse of the reduced stiffness matrix to solve for nodal displacements
+
+solucao_nodal = zeros(n_nos, 1);
+for z = 1:1:length(vaux)
+    % Loop through nodes without boundary conditions
+    solucao_nodal(vaux(z)) = psis(z);
+    % Creating the total solution vector including nodes where the solution is zero
+end
 
 end
 
--- code/Tensoes.m
+++ code/Tensoes.m
@@ -1,17 +1,19 @@
 function [centro_max,centro_min,tensao_max,tensao_min,tensaoxy,tensaoxz,tensaoyz] = Tensoes(n_elementos, matriz_dos_nos, matriz_de_incidencias,a,b,J,solucao_nodal)
-%funcao que calcula a intensidade das tensoes de corte existentes e
-%localiza a posiçao da tensao máxima e minima
+% Function to calculate shear stress intensities and locate maximum and minimum stresses
 
-tensaoxz = zeros(n_elementos,1); %inicialização a zeros de um array com n_elementos 
-tensaoxy = zeros(n_elementos,1); %inicialização a zeros de um array com n_elementos
-tensaoyz = zeros(n_elementos,1); %inicialização a zeros de um array com n_elementos
 
-for i = 1:n_elementos %percorre até ao numero de elementos
+tensaoxz = zeros(n_elementos,1); % Initialization of array XZ for shear stresses
+tensaoxy = zeros(n_elementos,1); % Initialization of array XY for shear stresses
+tensaoyz = zeros(n_elementos,1); % Initialization of array YZ for shear stresses
+
+for i = 1:n_elementos % Loop through the elements
     
-    x = a(i)/2; %coordenadas do centro
+    x = a(i)/2; % Coordinates of the center
     y = b(i)/2;
-    df1y = (1-x/a(i))*(-1/b(i));  %definição das derivadas da função de                                     
-    df2y = (-x/(a(i)*b(i)));      %forma para somar no vetor
+
+    % Definition of derivatives of the shape function
+    df1y = (1-x/a(i))*(-1/b(i));                                    
+    df2y = (-x/(a(i)*b(i)));      
     df3y = x/(a(i)*b(i));
     df4y = (1-x/a(i))*1/b(i);
     
@@ -20,7 +22,7 @@ for i = 1:n_elementos %percorre até ao numero de elementos
     df3x = y/(a(i)*b(i));
     df4x = -y/(a(i)*b(i));
     
-    %calculo das tensoes a partir das formulas teóricas (ver relatório)
+    % Calculation of shear stresses using theoretical formulas
     
     tensaoxz(i) = (1/J)*(solucao_nodal(matriz_de_incidencias(i,1))*df1y...
     + solucao_nodal(matriz_de_incidencias(i,2))*df2y + solucao_nodal(matriz_de_incidencias(i,3))*df3y...
@@ -30,27 +32,28 @@ for i = 1:n_elementos %percorre até ao numero de elementos
     + solucao_nodal(matriz_de_incidencias(i,2))*df2x + solucao_nodal(matriz_de_incidencias(i,3))*df3x...
     + solucao_nodal(matriz_de_incidencias(i,4))*df4x);
     
-    %determinação dos módulos de tensão obtido pela soma das duas tensões
+    % Calculation of the absolute shear stress
     tensaoxy(i) = (sqrt((tensaoxz(i))^2 + (tensaoyz(i))^2));
 end
 
 
-tensao_max = max(tensaoxy);  %cálculo da tensão máxima
-tensao_min = min(tensaoxy);  %cálculo da tensão minima
+tensao_max = max(tensaoxy);  % Calculation of maximum shear stress
+tensao_min = min(tensaoxy);  % Calculation of minimum shear stress
 
-for j = 1:n_elementos %percorre até nº elementos
+for j = 1:n_elementos % Loop through the elements
+    % Finding the elements with maximum and minimum stresses
     if tensaoxy(j) == tensao_max
-        emax = j; %obtenção do numero do elemento onde a tensão é máxima
+        emax = j; % Element number with maximum stress
     end
     if tensaoxy(j) == tensao_min
-        emin = j;  %obtenção do numero do elemento onde a tensão é minima
+        emin = j;  % Element number with minimum stress
     end
 end
 
-%cálculo do centro do elemento onde existe a tensão máxima e mínima
-centro_max(1) = matriz_dos_nos(matriz_de_incidencias(emax,2),2)-(a(emax))/2;              %obtenção das coordenadas do centro geométrico do elemento que contém a tensão máxima
+% Calculation of the centers of the elements with maximum and minimum stresses
+centro_max(1) = matriz_dos_nos(matriz_de_incidencias(emax,2),2)-(a(emax))/2;              
 centro_max(2) = matriz_dos_nos(matriz_de_incidencias(emax,4),3)-(b(emax))/2;
-centro_min(1) = matriz_dos_nos(matriz_de_incidencias(emin,2),2)-(a(emin))/2;              %obtenção das coordenadas do centro geométrico do elemento que contém a tensão minima
+centro_min(1) = matriz_dos_nos(matriz_de_incidencias(emin,2),2)-(a(emin))/2;              
 centro_min(2) = matriz_dos_nos(matriz_de_incidencias(emin,4),3)-(b(emin))/2;
 
 end
